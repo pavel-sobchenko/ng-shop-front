d602a93fe81b95cd6e060da092c274158cec56a7
Pavel Sobchanka <pavel.sobchanka@cortlex.com>
2022-05-10 13:15:22 +0200

    added styles

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@bluebits/ui';
+
+import { AccordionModule } from 'primeng/accordion';
+
+const ROUTES: Routes = [
+    {
+        path: '',
+        component: HomePageComponent,
+    },
+    {
+        path: 'products',
+        component: ProductListComponent,
+    },
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(ROUTES), UiModule, AccordionModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
